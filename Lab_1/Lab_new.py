48c76e139f2e612d4a29d967ed15083a7845e1dd
Lalalalex <a31294943@gmail.com>
2023-07-13 20:54:55 +0800

    Lab_1, third commit. Set seed/Loss function

--- Lab_1/Lab_new.py
+++ Lab_1/Lab_new.py
@@ -1,12 +1,26 @@
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import random
+import math
 
 class Cfg:
+    seed = '312551116'
     lr = 0.05
     activate = 'Sigmoid'
     schedular = 'WarmupLinear'
     epoch = 50000
+    warmup_step = 20000
+    loss_function = 'MSE'
+
+def setSeed(seed = Cfg.seed):
+    seed = math.prod([ord(i) for i in seed])%(2**32)
+    random.seed(seed)
+    np.random.seed(seed)
+
+setSeed(seed = Cfg.seed)
 
 def generate_linear(n = 100):
     pts = np.random.uniform(0, 1, (n, 2))
@@ -35,6 +49,17 @@ def generate_XOR_easy():
         labels.append(1)
 
     return np.array(inputs), np.array(labels).reshape(21,1)
+
+data_linear_train = {}
+data_linear_train['input'], data_linear_train['label'] = generate_linear(n = 300)
+data_XOR_train = {}
+data_XOR_train['input'], data_XOR_train['label'] = generate_XOR_easy()
+
+data_linear_test = {}
+data_linear_test['input'], data_linear_test['label'] = generate_linear()
+data_XOR_test = {}
+data_XOR_test['input'], data_XOR_test['label'] = generate_XOR_easy()
+
 def show_result(x, y, pred_y, file_name = 'result.png'):
     plt.figure()
     plt.subplot(1, 2, 1)
@@ -53,7 +78,7 @@ def show_result(x, y, pred_y, file_name = 'result.png'):
         else:
             plt.plot(x[i][0], x[i][1], 'bo')
     plt.savefig(file_name)
-def show_curve(loss, file_name = 'loss', title = 'Learning Curve', x ='epoch', y = 'loss'):
+def show_curve(loss, file_name = 'loss', title = 'Loss Curve', x ='epoch', y = 'loss'):
     plt.figure()
     plt.title(title)
     plt.plot(loss)
@@ -85,6 +110,25 @@ def d_activate(x, activate = Cfg.activate):
         return activation_functions[activate](x)
     else:
         print('d_Activation function not found.')
+
+def cal_loss(predict, label, loss_function = Cfg.loss_function):
+    loss_functions = {
+        'MSE': lambda label, predict: 1/2*np.mean((predict - label)**2),
+        'Binary cross entropy': lambda label, predict: -np.mean(label * np.log(predict) + (1 - label) * np.log(1 - predict))
+    }
+    if loss_function in loss_functions:
+        return loss_functions[loss_function](label, predict)
+    else:
+        print('Loss function not found.')
+def d_loss(predict, label, loss_function = Cfg.loss_function):
+    loss_functions = {
+        'MSE' : lambda label, predict : -(label - predict)/len(label.shape),
+        'Binary cross entropy': lambda label, predict: (predict - label) / (predict * (1 - predict))
+    }
+    if loss_function in loss_functions:
+        return loss_functions[loss_function](label, predict)
+    else:
+        print('d_Loss function not found.')
 
 class Layer:
     def __init__(self, input_number, output_number, activate = Cfg.activate):
@@ -135,7 +179,7 @@ class Network:
         schedular_list = {
             'None': lambda lr: lr,
             'ExponentialLR': lambda lr: gamma * lr if step % 1000 == 0 else lr,
-            'PolynomialLR': lambda lr: lr - gamma,
+            'PolynomialLR': lambda lr: lr - gamma if step % 1000 == 0 else lr,
             'WarmupLinear': lambda lr: float(step) / float(max(1, warmup_step)) \
             if step < warmup_step \
             else max(0.0, float(total_step - step)) / float(max(1.0, total_step - warmup_step)),
@@ -144,26 +188,22 @@ class Network:
             self.lr =  schedular_list[schedular](self.lr)
         else:
             print('Schedular function not found.')
-    def loss_function(self, predict, label):
-        return 1/2*np.mean((predict - label)**2)
-    def d_loss(self, predict, label):
-        return -(label - predict)/len(label.shape)
-    
+  
 def predict2label(predict, threshold = 0.5):
     return np.where(predict > threshold, 1, 0)
 
 lr = []
-def train(model, data, epoch = Cfg.epoch):
+def train(model, data, warmup_step = Cfg.warmup_step, schedular = Cfg.schedular, epoch = Cfg.epoch):
     total_loss = []
     with tqdm(range(epoch)) as tqdm_loader:
         for i in tqdm_loader:
             lr.append(model.lr)
             predict = predict2label(model.forward(data["input"]))
-            loss = model.loss_function(predict, data['label'])
+            loss = cal_loss(predict, data['label'])
             total_loss.append(loss)
-            model.backward(model.d_loss(predict, data['label']))
+            model.backward(d_loss(predict, data['label']))
             model.update()
-            model.schedular_step(schedular = Cfg.schedular, step = i, total_step = epoch)
+            model.schedular_step(schedular = schedular, step = i, total_step = epoch, warmup_step = warmup_step)
             accuracy = np.sum(predict == data['label']) / len(data['label'])
             tqdm_loader.set_postfix(epoch = i, loss = loss, lr = model.lr, accuracy = accuracy)
         return total_loss
@@ -172,21 +212,16 @@ def test(model, data):
     predict = predict2label(model.forward(data["input"]))
     return predict
 
-data_linear = {}
-data_linear['input'], data_linear['label'] = generate_linear()
-model_linear = Network(input_number = 2, output_number = 1, hidden_number = 2, hidden_size = 4)
-total_loss_linear = train(model_linear, data_linear)
-predict = test(model_linear, data_linear)
-show_result(data_linear['input'], data_linear['label'], predict, file_name = 'linear')
+model_linear = Network(input_number = 2, output_number = 1, hidden_number = 2, hidden_size = 4, lr = 0.5)
+total_loss_linear = train(model_linear, data_linear_train, epoch = 10000, warmup_step = 3000)
+predict = test(model_linear, data_linear_test)
+show_result(data_linear_test['input'], data_linear_test['label'], predict, file_name = 'linear')
 show_curve(total_loss_linear, file_name = 'linear_loss')
-
 
 show_curve(lr, file_name = 'learning_rate', title = 'learning rate curve', x = 'epoch', y = 'lr')
 
-data_XOR = {}
-data_XOR['input'], data_XOR['label'] = generate_XOR_easy()
 model_XOR = Network(input_number = 2, output_number = 1, hidden_number = 2, hidden_size = 4)
-total_loss_XOR = train(model_XOR, data_XOR)
-predict = test(model_XOR, data_XOR)
-show_result(data_XOR['input'], data_XOR['label'], predict, file_name = 'XOR')
+total_loss_XOR = train(model_XOR, data_XOR_train, epoch = 15000, warmup_step = 4000)
+predict = test(model_XOR, data_XOR_test)
+show_result(data_XOR_test['input'], data_XOR_test['label'], predict, file_name = 'XOR')
 show_curve(total_loss_XOR, file_name = 'XOR_loss')
